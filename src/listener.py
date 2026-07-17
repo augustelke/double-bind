8243231f035f6948279bc9f681ae7b00f7d8ee9c
augustelke <auguste.marcorelles.5@gmail.com>
2026-07-17 11:03:20 +0200

    Implement key press simulation in listener.py

--- src/listener.py
+++ src/listener.py
@@ -1,1 +1,18 @@
+import keyboard
+import time
 
+from src.settings import INPUT_KEY, OUTPUT_KEY, CHECK_DELAY
+from src.simulator import press_key, release_key
+
+
+def start():
+
+    while True:
+
+        if keyboard.is_pressed(INPUT_KEY):
+            press_key(OUTPUT_KEY)
+
+        else:
+            release_key(OUTPUT_KEY)
+
+        time.sleep(CHECK_DELAY)
