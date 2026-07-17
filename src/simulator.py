06d614a3f5426048869dc8f6b931f404e5732aff
augustelke <auguste.marcorelles.5@gmail.com>
2026-07-17 11:02:59 +0200

    Add key press and release functions

--- src/simulator.py
+++ src/simulator.py
@@ -1,1 +1,19 @@
+import keyboard
 
+pressed = False
+
+
+def press_key(key):
+    global pressed
+
+    if not pressed:
+        keyboard.press(key)
+        pressed = True
+
+
+def release_key(key):
+    global pressed
+
+    if pressed:
+        keyboard.release(key)
+        pressed = False
